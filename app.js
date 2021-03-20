40e3c5abdf86abd29928446c943f00def1bf2b04
Ananthan R <ananthan.rajasekharan@gmail.com>
2021-03-20 12:25:12 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,53 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+Web3 = require("web3");
+
+var MyContractJSON =require(path.join(__dirname, 'build/contracts/certi.json'))
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+const web3 = new Web3('http://localhost:8545');
+
+accountAddress = "0x35218bA732a98A848b776a7f13F31a792B2DB94B";
+contractAddress = MyContractJSON.networks['5777'].address;
+var contractAbi = MyContractJSON.abi;
+
+MyContract = new web3.eth.Contract(contractAbi, contractAddress);
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/view', function(req, res, next) {
+  data = {"courseName" : "Certified Ethereum Developer", "certificateID" : "KBACED1001", "cadndidateName" : "Ananthan", "grade" : "S", "date" : "2021-02-20"}
+  res.render('viewCertificate', {data: data});
+});
+
+router.post('/view', function(req, res, next) {
+  data = req.body;
+  console.log(data);
+  // res.send(data)
+  MyContract.methods.certificateDetails(data.certificateID)
+    .call({from:accountAddress})
+    .then((result) => {
+        console.log(result);
+        res.render('viewCertificate', {data : result});
+    })
+});
+
+router.get('/issue', function(req, res, next) {
+  res.render('issueCertificate', {formClass : '', messageClass : 'hidden', certificateID : ''});
+});
+
+router.post('/issue', function(req, res, next) {
+  data = req.body;
+  console.log(data);
+  // res.send(data)
+
+  MyContract.methods.newCertificate(data.certificateID, data.courseName, data.cadndidateName, data.grade, data.date)
+  .send({from : accountAddress, gasLimit : "927000"}).then((txn) => {
+    res.render('issueCertificate', {formClass : 'hidden', messageClass : '', certificateID : data.certificateID})
+  })
+
+});
+
+module.exports = router;
